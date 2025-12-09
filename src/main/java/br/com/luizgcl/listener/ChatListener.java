7bc54d1795fc5a3fa361cc46b6d510b6a5d156f9
Luiz Gustavo <62274995+luizgcl@users.noreply.github.com>
2025-12-09 18:09:31 -0300

    melhorias no chat

--- src/main/java/br/com/luizgcl/listener/ChatListener.java
+++ src/main/java/br/com/luizgcl/listener/ChatListener.java
@@ -5,15 +5,16 @@ import org.bukkit.event.Listener;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 public class ChatListener implements Listener {
 
     @EventHandler
     void event(AsyncChatEvent event) {
-        event.message(
-            event.getPlayer().displayName()
+        event.renderer((source, sourceDisplayName, message, viewer) -> 
+            source.displayName().colorIfAbsent(NamedTextColor.GRAY)
             .append(Component.text(" §8» §f" ))
-            .append(event.message())
+            .append(message)
         );
     }
 }
